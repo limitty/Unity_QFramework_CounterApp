82da67336d807a611c0ce76ccd3a4b0c04db060c
limitty <tabeworks@gmail.com>
2025-06-30 12:21:35 +0900

    Introduce QFramework and Introduce Model and Architecture

--- Assets/Scripts/CounterAppController.cs
+++ Assets/Scripts/CounterAppController.cs
@@ -2,42 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using QFramework;
 
-public class CounterAppController : MonoBehaviour
+namespace MyCounterApp
 {
-    // View
-    private Button mBtnAdd;
-    private Button mBtnSub;
-    private Text mCountText;
-    
-    // Model
-    private int mCount = 0;
-    
-    void Start()
+    public class CounterAppModel : AbstractModel
     {
-        // Get UI components
-        mBtnAdd = transform.Find("BtnAdd").GetComponent<Button>();
-        mBtnSub = transform.Find("BtnSub").GetComponent<Button>();
-        mCountText = transform.Find("CountText").GetComponent<Text>();
-        
-        mBtnAdd.onClick.AddListener(() =>
+        public int Count;
+        protected override void OnInit()
         {
-            mCount++;
-            UpdateView();
-        });
-        
-        mBtnSub.onClick.AddListener(() =>
-        {
-            mCount--;
-            UpdateView();
-        });
-        
-        UpdateView();
+            Count = 0;
+        }
     }
 
-    private void UpdateView()
+    public class CounterApp : Architecture<CounterApp>
     {
-        mCountText.text = mCount.ToString();
+        protected override void Init()
+        {
+            this.RegisterModel(new CounterAppModel());
+        }
     }
     
+    public class CounterAppController : MonoBehaviour, IController
+    {
+        // View
+        private Button mBtnAdd;
+        private Button mBtnSub;
+        private Text mCountText;
+        
+        // Model
+        private CounterAppModel mModel;
+        
+        void Start()
+        {
+            mModel = this.GetModel<CounterAppModel>();
+            // Get UI components
+            mBtnAdd = transform.Find("BtnAdd").GetComponent<Button>();
+            mBtnSub = transform.Find("BtnSub").GetComponent<Button>();
+            mCountText = transform.Find("CountText").GetComponent<Text>();
+            
+            mBtnAdd.onClick.AddListener(() =>
+            {
+                mModel.Count++;
+                UpdateView();
+            });
+            
+            mBtnSub.onClick.AddListener(() =>
+            {
+                mModel.Count--;
+                UpdateView();
+            });
+            
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            mCountText.text = mModel.Count.ToString();
+        }
+
+        public IArchitecture GetArchitecture()
+        {
+            return CounterApp.Interface;
+        }
+
+        private void OnDestroy()
+        {
+            mModel = null;
+        }
+        
+    }
 }
